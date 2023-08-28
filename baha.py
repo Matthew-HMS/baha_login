58df8fb8b7815eab8f9dff084fa77c53da080267
imsho <imsho0218@gmail.com>
2023-08-28 22:03:26 +0800

    first commit

---
+++ baha.py
@@ -0,0 +1,49 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pickle
+from selenium.common.exceptions import NoSuchElementException
+
+account = '54matt'
+password = 'Ho0218ho'
+
+options = webdriver.ChromeOptions()
+# options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.gamer.com.tw/")
+
+# Load cookie
+cookies = pickle.load(open("gamer_cookies.pkl", "rb"))
+for cookie in cookies:
+    driver.add_cookie(cookie)
+driver.refresh()
+
+# check login
+try:
+    driver.find_element(By.XPATH, "//a[@href='https://user.gamer.com.tw/login.php']")
+    login = False
+    print("not login")
+except NoSuchElementException:
+    login = True
+
+if login:
+    print("login")
+else:
+    driver.get("https://user.gamer.com.tw/login.php")
+    driver.refresh()
+    account = driver.find_element(By.XPATH, "//*[@name = 'userid']")
+    account.send_keys('matthewhms')
+    pword = driver.find_element(By.XPATH, "//*[@name = 'password']")
+    pword.send_keys('ho0218ho')
+    time.sleep(5)
+    submit = driver.find_element(By.XPATH, "//*[@id = 'btn-login']")
+    submit.click()
+
+    # save cookie
+    time.sleep(5)
+    pickle.dump(driver.get_cookies(), open("gamer_cookies.pkl", "wb"))
+
+
+# quit
+time.sleep(10)
+driver.quit()
