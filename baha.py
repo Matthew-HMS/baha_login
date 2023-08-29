0e6a03eeffcc09948c9415c35fa307d42de5e031
imsho <imsho0218@gmail.com>
2023-08-29 11:03:25 +0800

    [fix] baha don' t need cookie

--- baha.py
+++ baha.py
@@ -13,38 +13,27 @@ options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.gamer.com.tw/")
 
-# Load cookie
-cookies = pickle.load(open("baha_cookies.pkl", "rb"))
-for cookie in cookies:
-    driver.add_cookie(cookie)
+# Login
+driver.get("https://user.gamer.com.tw/login.php")
 driver.refresh()
+email = driver.find_element(By.XPATH, "//*[@name = 'userid']")
+email.send_keys(account)
+pword = driver.find_element(By.XPATH, "//*[@name = 'password']")
+pword.send_keys(password)
+time.sleep(5)
+submit = driver.find_element(By.XPATH, "//*[@id = 'btn-login']")
+submit.click()
 
-# check login
-try:
-    driver.find_element(By.XPATH, "//a[@href='https://user.gamer.com.tw/login.php']")
-    login = False
-    print("not login")
-except NoSuchElementException:
-    login = True
-
-if login:
-    print("login")
-else:
-    driver.get("https://user.gamer.com.tw/login.php")
-    driver.refresh()
-    email = driver.find_element(By.XPATH, "//*[@name = 'userid']")
-    email.send_keys(account)
-    pword = driver.find_element(By.XPATH, "//*[@name = 'password']")
-    pword.send_keys(password)
-    time.sleep(5)
-    submit = driver.find_element(By.XPATH, "//*[@id = 'btn-login']")
-    submit.click()
-
-    # save cookie
-    time.sleep(5)
-    pickle.dump(driver.get_cookies(), open("baha_cookies.pkl", "wb"))
-
+# double reward
+time.sleep(2)
+driver.get("https://www.gamer.com.tw/")
+time.sleep(3)
+daily = driver.find_element(By.XPATH, "//*[@id='signin-btn']")
+daily.click()
+double = driver.find_element(By.XPATH, "//*[@class='popup-dailybox__btn']")
+double.click()
+time.sleep(5)
 
 # quit
-time.sleep(10)
+time.sleep(30)
 driver.quit()
