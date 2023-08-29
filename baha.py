f497b845b0200551b9d5ffd36010baae7aba92db
imsho <imsho0218@gmail.com>
2023-08-29 11:15:33 +0800

    [fix] none stop

--- baha.py
+++ baha.py
@@ -30,9 +30,12 @@ driver.get("https://www.gamer.com.tw/")
 time.sleep(3)
 daily = driver.find_element(By.XPATH, "//*[@id='signin-btn']")
 daily.click()
+time.sleep(3)
 double = driver.find_element(By.XPATH, "//*[@class='popup-dailybox__btn']")
-double.click()
-time.sleep(5)
+try:
+    double.click()
+except:
+    pass
 
 # quit
 time.sleep(30)
